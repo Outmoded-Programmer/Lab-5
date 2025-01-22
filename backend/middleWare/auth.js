f23c5f7967d7c7e755bfd48be0552883bed4bb97
Taha-Asad <tahaasad709@gmail.com>
2025-01-22 21:23:49 +0500

    ....

---
+++ backend/middleWare/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+function authenticationToken(req , res , next){
+    const token = req.headers['authorization'];
+    if(!token){
+        return res.status(404).send("Acess denied");
+    }
+    jwt.verify(token , process.env.JWT_SECRET , (error , user)=>{
+        if(error){
+            return res.status(403).send("Invalid Token");
+        }
+        req.user = user ;
+        next()
+    });
+};
+
+
+function authorizeRole(...roles){
+    return(req , res, next)=>{
+        if(!roles.includles(req.user.role) || req.user.role !== 'admin'){
+            return res.status(404).send("Access deined!!!")
+        }
+        next()
+    }
+}
+
+
+module.exports = {authenticationToken , authorizeRole}
---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { isStrongPassword } = require('validator');
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        require:true,
+        unique: true ,
+    },
+    email:{
+        type:String,
+        unique: true,
+        require:true
+    },
+    password:{
+        type: String,
+        unique:true ,
+        require:true,
+        validate: function(value){
+           return isStrongPassword(value);
+        },
+        message:"Password is not strong enough", 
+    },
+    role:{
+        type: String,
+        enum:['admin' , 'user'] ,
+        default: 'user' ,
+    }
+})
+
+module.exports = mongoose.model('User' , userSchema)
