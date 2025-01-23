59e43320f9f64bf7cc0a98e56ea1dd4e4d7a911a
Taha-Asad <tahaasad709@gmail.com>
2025-01-23 10:18:27 +0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,21 @@
+const User = require("../models/userModel")
+
 const getUser = async(req , res)=>{
     try {
-        
+        const user = await User.findById(req.user.Id)
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json(error.message)     
     }
 }
+
+const postUser = async(req , res)=>{
+    const {userName ,  email , password , role} = await req.body
+    try{
+        const user = 
+    }
+}
+
+
+
+module.exports = {getUser}
