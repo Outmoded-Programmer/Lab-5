46953db0541d6106ec8a3de9090e4fd3129b6cc2
Taha-Asad <tahaasad709@gmail.com>
2025-02-06 00:41:21 +0500

    Create Home.jsx

---
+++ frontend/react-app/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Components/Body/Hero";
+import CategoryCar from "./Components/Body/CategoryCar";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <CategoryCar />
+    </>
+  );
+};
+
+export default Home;
