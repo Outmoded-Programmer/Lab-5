3d696f24c02dee9989ad0076f2e6bd6ff1a9f950
Taha-Asad <tahaasad709@gmail.com>
2025-02-06 00:41:38 +0500

    Update categoryModel.js

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -5,6 +5,10 @@ const categorySchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+  image:{
+    type:String ,
+    require:true ,
+  },
   food: [
     {
       type: mongoose.Schema.Types.ObjectId,
